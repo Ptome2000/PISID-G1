5e15352edd7bd774e8c7bd7fa370aabb24a25409
ademirs94 <ademir.s.94@hotmail.com>
2025-03-18 22:18:15 +0000

    finalização do script

--- send_from_mongo.py
+++ send_from_mongo.py
@@ -1,27 +1,50 @@
 import json
-import re
+import time
 import paho.mqtt.client as mqtt
-import pymongo
 from pymongo import MongoClient
+import threading
 client = MongoClient("localhost", 27017)
 
 db = client["labirinto_pisid"]
 
 
+def start_game(player):
+    print("Esta é uma thread")
+    while True:
+        sendData("movimento", player)
+        sendData("ruido", player)
+        time.sleep(0.5)  # Espera 0.5 segundos antes da próxima execução
+
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    client.subscribe("pisid_g1_movimento_1")
+    client.subscribe("pisid_g1_player")
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+    data = json.loads(msg.payload)
+    if msg.topic == "pisid_g1_player":
+        thread = threading.Thread(target=start_game, args=(data["player"],))
+        thread.start()
 
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
+mqttc.on_message = on_message
 mqttc.connect("broker.mqtt-dashboard.com", 1883, 60)
 
 
-def sendData(collectionName):
+def sendData(collectionName, player):
+    print("player", player, collectionName)
     collection = db[collectionName]
-    query = {"$or": [{"isRead": False}, {"isRead": {"$exists": False}}]}
+    query = {
+        "$and": [
+            {"Player": player},
+            {"$or": [{"isRead": False}, {"isRead": {"$exists": False}}]}
+        ]
+    }
     data = collection.find(query)
+
+    topic = f"pisid_g1_{collectionName}_{player}"
     for row in data:
         mongo_id = row["_id"]  # Guarda o _id antes de remover, para usar mais tarde ao fazer o update
         del row["_id"]  # Remove o _id do objeto antes do JSON dump
@@ -29,10 +52,10 @@ def sendData(collectionName):
         # Verificar o que é spam e o que não é
 
         # enviar para o MQTT este registo
-        mqttc.publish("pisid_g1_movimento_1", json.dumps(row), 2)
+        mqttc.publish(topic, json.dumps(row), 2)
         # atualizar na base de dados para isRead=True
         collection.update_one({"_id": mongo_id}, {"$set": {"isRead": True}})
 
 
-sendData("movimento")
+mqttc.loop_forever()
 
