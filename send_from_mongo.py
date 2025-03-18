acb8ee56ea0d56a9d02891219b7d3ce3b8bf8ae0
ademirs94 <ademir.s.94@hotmail.com>
2025-03-18 21:17:45 +0000

    wip

--- send_from_mongo.py
+++ send_from_mongo.py
@@ -6,28 +6,33 @@ from pymongo import MongoClient
 client = MongoClient("localhost", 27017)
 
 db = client["labirinto_pisid"]
-collection = db["movimento"]
 
-query = {"$or": [{"isRead": False}, {"isRead": {"$exists": False}}]}
-movimentos = collection.find(query, {"_id": 0})
 
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
     client.subscribe("pisid_g1_movimento_1")
+
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.connect("broker.mqtt-dashboard.com", 1883, 60)
 
-for x in movimentos:
-    print(x)
-    # Verificar o que é spam e o que não é
 
-    # enviar para o MQTT este registo
-    mqttc.publish("pisid_g1_movimento_1", json.dumps(x), 2)
-    # atualizar na base de dados para isRead=True
+def sendData(collectionName):
+    collection = db[collectionName]
+    query = {"$or": [{"isRead": False}, {"isRead": {"$exists": False}}]}
+    data = collection.find(query)
+    for row in data:
+        mongo_id = row["_id"]  # Guarda o _id antes de remover, para usar mais tarde ao fazer o update
+        del row["_id"]  # Remove o _id do objeto antes do JSON dump
+        print(row)
+        # Verificar o que é spam e o que não é
+
+        # enviar para o MQTT este registo
+        mqttc.publish("pisid_g1_movimento_1", json.dumps(row), 2)
+        # atualizar na base de dados para isRead=True
+        collection.update_one({"_id": mongo_id}, {"$set": {"isRead": True}})
 
 
+sendData("movimento")
 
