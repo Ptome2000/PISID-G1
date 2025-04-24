2c09a057aa016bd6cb4bbc1186b82c49b9c4f20a
ademirs94 <ademir.s.94@hotmail.com>
2025-04-24 01:31:44 +0100

    script para correr jogo no pc1

---
+++ scripts/run_pc1.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+current_player = os.getenv("CURRENT_PLAYER", 1) # current player
+
+print('starting replicas...')
+subprocess.run('startReplicaSet.bat')
+
+print('starting Script1: mqtt_to_mongo_ingest.py')
+mqtt_proc = subprocess.Popen(
+    f'start "MQTT To Mongo - Ingest" python ../src/mqtt_to_mongo_ingest.py',
+    shell=True,
+    creationflags=subprocess.CREATE_NEW_CONSOLE,
+)
+
+print('starting Script2: mongo_to_mqtt_publisher.py')
+mongo_proc = subprocess.Popen(
+    f'start "Mongo To MQTT - Publisher" python ../src/mongo_to_mqtt_publisher.py',
+    shell=True,
+    creationflags=subprocess.CREATE_NEW_CONSOLE,
+)
+
+print('starting Game mazerun...')
+mazerun_proc = subprocess.Popen(
+    f'start "Mazerun" ../game/mazerun.exe {current_player}',
+    shell=True,
+    creationflags=subprocess.CREATE_NEW_CONSOLE,
+)
+
+
