99a2c0a81b384d0f0c27a610d9d28c9a615e5de7
ademirs94 <ademir.s.94@hotmail.com>
2025-05-02 18:40:07 +0100

    php: login

--- src/test_mqtt.py
+++ src/test_mqtt.py
@@ -4,17 +4,15 @@ mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    '''
-    client.subscribe("pisid_g1_ruido_1")
+    client.subscribe("pisid_mazesound_1")
     for i in range(500):
-        res = mqttc.publish("pisid_g1_ruido_1", '{Player:1, Hour:"2024-07-04 16:29:21.281898", Sound:19.0}')
+        res = mqttc.publish("pisid_mazesound_1", '{Player:1, Hour:"2024-07-04 16:29:21.281898", Sound:19.0}')
         print(f'enviado {i} {res}')
-    '''
 
-    client.subscribe("pisid_g1_movimento_1")
-    for i in range(500):
-        res = mqttc.publish("pisid_g1_movimento_1", '{"Player": 1, "Marsami": 47, "RoomOrigin": 4, "RoomDestiny": 5, "Status": 1}')
-        print(f'enviado {i} {res}')
+    # client.subscribe("pisid_g1_movimento_1")
+    # for i in range(500):
+    #     res = mqttc.publish("pisid_g1_movimento_1", '{"Player": 1, "Marsami": 47, "RoomOrigin": 4, "RoomDestiny": 5, "Status": 1}')
+    #     print(f'enviado {i} {res}')
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
