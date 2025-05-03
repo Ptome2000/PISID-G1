3fd995d0d3d06fb025ee3eea2227ddc8b0ecb5bd
David Fernandes <david.fernandes.ti@gmail.com>
2025-05-03 19:26:49 +0100

    Alteração do SP criar_utilizador

--- MySQL/Functions/criar_utilizador.sql
+++ MySQL/Functions/criar_utilizador.sql
@@ -1,3 +1,5 @@
+DELIMITER //
+
 CREATE DEFINER=`app`@`localhost` PROCEDURE `criar_utilizador`(
     IN `name` VARCHAR(100),
     IN `username` VARCHAR(50),
@@ -7,13 +9,33 @@ CREATE DEFINER=`app`@`localhost` PROCEDURE `criar_utilizador`(
 )
 SQL SECURITY DEFINER
 BEGIN
-    -- Cria o utilizador MySQL com password fornecida
-    SET @create_user = CONCAT('CREATE USER \'', username, '\'@\'localhost\' IDENTIFIED BY \'', password, '\';');
+    -- Cria o utilizador MySQL se não existir
+    SET @create_user = CONCAT(
+        'CREATE USER IF NOT EXISTS \'', username, '\'@\'localhost\' IDENTIFIED BY \'', password, '\';'
+    );
     PREPARE stmt FROM @create_user;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
-    -- Insere os dados na tabela da aplicação, sem password
-    INSERT INTO `user` (`Nome`, `Username`, `Email`, `Telemovel`)
-    VALUES (name, username, email, phone);
-END
+    -- Dá permissão de leitura (SELECT) na base de dados marsami_game
+    SET @grant_select = CONCAT(
+        'GRANT SELECT ON marsami_game.* TO \'', username, '\'@\'localhost\';'
+    );
+    PREPARE stmt FROM @grant_select;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+
+    -- Dá permissão de execução da procedure marsami_game.ver_dados
+    SET @grant_proc1 = CONCAT(
+        'GRANT EXECUTE ON PROCEDURE marsami_game.alterar_jogo TO \'', username, '\'@\'localhost\';'
+    );
+    PREPARE stmt FROM @grant_proc1;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+
+    -- Insere os dados na tabela da aplicação
+    INSERT INTO `user` (`Username`, `Nome`, `Telemovel`, `Tipo`, `Email`, `Grupo`)
+    VALUES (username, name, phone, 'USR', email, 1);
+END //
+
+DELIMITER ;
